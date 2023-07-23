56d1964c3fb30c3f5ce5e2a5fe15339506d677b1
jericho909 <omerahmetyucel@gmail.com>
2023-07-23 12:36:27 +0300

    edited tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let c = 0;
+    let newStr = "";
+    if (num < 0) {
+        let err = "ERROR";
+        return err
+    } else {
+        while (c<num) {
+            newStr = newStr + str
+            c += 1;
+        }
+    
+        return newStr
 
+    }
+    
 };
 
 // Do not edit below this line
