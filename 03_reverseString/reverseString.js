b59f86bfaa1cc812fc07b3ae59fe4896101c32c8
jericho909 <omerahmetyucel@gmail.com>
2023-07-23 12:48:25 +0300

    edited the tests and main function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split('');
+    let reverseString = splitString.reverse();
+    let joinedString = reverseString.join("");
 
+    return joinedString
+    
 };
 
 // Do not edit below this line
