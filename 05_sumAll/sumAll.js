8b1970fa070e1f7d8d67d86e0da7fd5185c31e5c
jericho909 <omerahmetyucel@gmail.com>
2023-07-24 12:13:46 +0300

    edited the function and tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ((a < 0 || b < 0) || ( typeof(a) !== "number" || typeof(b) !== "number" )) {
+        let err = "ERROR";
+        return err
+    }
+
+    if (a> b) {
+    let sum = ((a) * (a+1)) / 2;
+    return sum
+    } else {
+    let sum = ((b) * (b+1)) / 2;
+    return sum
+    }
+
+    
 
 };
 
