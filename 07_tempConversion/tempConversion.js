21c83413a7426a730456746f3fe6c9789c6ecc4a
jericho909 <omerahmetyucel@gmail.com>
2023-07-24 12:29:26 +0300

    edited function and tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celcius) {
+  let converted = ((celcius - 32) * (5 / 9)).toFixed(1);
+  converted = parseFloat(converted);
+  return converted
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheit) {
+  let converted = ((fahrenheit * (9 / 5) + 32)).toFixed(1);
+  converted = parseFloat(converted);
+  return converted
+  
 };
 
 // Do not edit below this line
