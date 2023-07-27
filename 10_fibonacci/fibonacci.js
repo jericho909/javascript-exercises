813cf1c867878fa932124ee4965ac62ddd03b88a
jericho909 <omerahmetyucel@gmail.com>
2023-07-27 16:43:47 +0300

    edited js logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let fib = [1];
+num = parseInt(num);
+if (num > 0) {
+    for (let i=0; i<num; i++){
+        if (fib.length < 3){
+            let nextNum = fib[i] + i;
+            fib.push(nextNum);
+        } else {
+            let nextNum = fib[i] + fib[i-1];
+            fib.push(nextNum);
+        }
+      };
+    
+    return fib[num-1]
+} else if (num === 0) {
+    return 0
+} else {
+    let err = "OOPS";
+    return err
+}
 
 };
 
